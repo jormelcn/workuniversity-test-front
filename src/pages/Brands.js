a398977c5f93b19c6e9eba9c6d837ae692bfd6a0
Jorge Narváez <jormelcn@gmail.com>
2021-03-01 11:06:07 -0500

    feat: mostrar marcas desde la api, listo.

--- src/pages/Brands.js
+++ src/pages/Brands.js
@@ -1,26 +1,46 @@
+import { useEffect, useState } from "react";
+
 import Edit from "../icons/Edit";
 import Delete from "../icons/Delete";
 
-const tipoVehiculos = [
-  {
-    id: "laialshdlahsd",
-    name: "Toyota",
-    manufacturingHours: 4,
-  },
-  {
-    id: "laialshdlahsdzcz",
-    name: "Ford",
-    manufacturingHours: 3,
-  },
-  {
-    id: "laialshdlahsdweqw",
-    name: "Chevrolet",
-    manufacturingHours: 2,
-  },
-];
+// const tipoVehiculos = [
+//   {
+//     id: "laialshdlahsd",
+//     name: "Toyota",
+//     manufacturingHours: 4,
+//   },
+//   {
+//     id: "laialshdlahsdzcz",
+//     name: "Ford",
+//     manufacturingHours: 3,
+//   },
+//   {
+//     id: "laialshdlahsdweqw",
+//     name: "Chevrolet",
+//     manufacturingHours: 2,
+//   },
+// ];
 
 
 export default function Marks() {
+
+  const [state, setstate] = useState([]);
+
+  useEffect(() => { 
+
+    const baseUrl = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_DEV_BASE_URL : process.env.REACT_APP_PRO_BASE_URL;
+
+    const getAllBrands = async () => {
+      fetch(`${baseUrl}/vehicle-type`)
+        .then(res => res.json() )
+        .then(json => setstate(json))    
+    };
+
+    getAllBrands()
+  
+  }, []);
+
+
   return (
     <section className="flex justify-between items-center gap-10">
       <div className="w-1/2 shadow p-4 shadow bg-white">
@@ -32,7 +52,7 @@ export default function Marks() {
             </tr>
           </thead>
           <tbody>
-            {tipoVehiculos.map((m, i) => (
+            {state.map((m, i) => (
               <tr key={i} className="4">
                 <td className=" font-medium  "> {m.name} </td>
                 <td className=" font-medium "> {m.manufacturingHours} </td>
